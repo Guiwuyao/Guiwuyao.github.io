3407eb74e93915840cc87296324163f7b323d40d
ljc <lijingcheng.lj@gmail.com>
2026-05-16 12:42:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-
+		{
+			name: "旧版",
+			url: "https://yongzhi-lee.github.io/", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
 	],
 };
 
