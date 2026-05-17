e19a0acd01d5baeaeb7ffd1b4825b75280d2fd1e
ljc <lijingcheng.lj@gmail.com>
2026-05-17 14:16:29 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -62,7 +62,7 @@ export const profileConfig: ProfileConfig = {
 			icon: "fa6-brands:user-group", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "/friends/",
+			url: "/friends",
 		},
 		{
 			name: "微信公众号",
